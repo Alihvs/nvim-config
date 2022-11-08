8c5274ced7ccc01ea9a01a3eef42d45b4c434373
Ali Havasi <alihavasi@Alis-MacBook-Pro.local>
2022-11-08 09:43:14 +0330

    feat: add new plugins

---
+++ lua/ali/plugins/treesitter.lua
@@ -0,0 +1,45 @@
+-- import nvim-treesitter plugin safely
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
+
+-- configure treesitter
+treesitter.setup({
+	-- enable syntax highlighting
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	-- enable indentation
+	indent = { enable = true },
+	-- enable autotagging (w/ nvim-ts-autotag plugin)
+	autotag = { enable = true },
+	-- ensure these language parsers are installed
+	ensure_installed = {
+		"json",
+		"javascript",
+		"typescript",
+		"tsx",
+		"yaml",
+		"html",
+		"css",
+		"markdown",
+		"svelte",
+		"graphql",
+		"bash",
+		"lua",
+		"vim",
+		"dockerfile",
+		"gitignore",
+		"rust",
+		"toml",
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true,
+		max_file_lines = nil,
+	},
+	-- auto install above language parsers
+	auto_install = true,
+})
