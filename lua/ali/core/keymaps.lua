d4adbfdba5c02154ce337148a9b1f535e7649269
Ali Havasi <alihvs@gmail.com>
2022-12-31 15:47:32 +0330

    chore: add keybinds for diffview

--- lua/ali/core/keymaps.lua
+++ lua/ali/core/keymaps.lua
@@ -59,3 +59,7 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+-- DiffView
+keymap.set("n", "<leader>gg", ":DiffviewOpen<CR>")
+keymap.set("n", "<leader>gc", ":DiffviewClose<CR>")
