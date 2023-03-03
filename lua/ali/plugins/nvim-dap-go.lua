4469798541d8f70be192620cf0b8f4d87b82c21f
Ali Havasi <alihvs@gmail.com>
2023-03-03 07:08:18 +0100

    chore: nvim dap

--- lua/ali/plugins/nvim-dap-go.lua
+++ lua/ali/plugins/nvim-dap-go.lua
@@ -8,11 +8,11 @@ dg.setup({
 	-- dap_configurations accepts a list of tables where each entry
 	-- represents a dap configuration. For more details do:
 	-- :help dap-configuration
-	dap_configurations = {
-		{
-			-- Must be "go" or it will be ignored by the plugin
+	{
+		dap_configurations = {
+			-- must be "go" or it will be ignored by the plugin
 			type = "go",
-			name = "Attach remote",
+			name = "attach remote",
 			mode = "remote",
 			request = "attach",
 		},
@@ -22,8 +22,8 @@ dg.setup({
 		-- time to wait for delve to initialize the debug session.
 		-- default to 20 seconds
 		initialize_timeout_sec = 20,
-		-- a string that defines the port to start delve debugger.
 		-- default to string "${port}" which instructs nvim-dap
+		-- a string that defines the port to start delve debugger.
 		-- to start the process in a random available port
 		port = "${port}",
 	},
